f47bde1a1c9f45d23e4cca5b869cb1e608a1d3b1
filipvabrousek <filipvabrousek1@gmail.com>
2025-07-16 20:35:20 +0200

    release: v1.0.0-beta.25

--- release.sh
+++ release.sh
@@ -3,5 +3,5 @@ cp -R /Users/filipvabrousek/Desktop/layout/lib /Users/filipvabrousek/launch/
 current_version=$(grep -o '"version": "[^"]*"' package.json | sed -E 's/"version": "([0-9]+\.[0-9]+\.[0-9]+-beta\.)([0-9]+)"/\2/'); \
 next_version=$((current_version + 1)); \
 sed -i '' -E "s/(\"version\": \"[0-9]+\.[0-9]+\.[0-9]+-beta\.)[0-9]+\"/\1${next_version}\"/" package.json
-
+npm run build
 npm publish
