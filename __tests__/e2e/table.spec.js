a41378593342c9eec754f9e18cb4f62bbd0520b4
filipvabrousek <filipvabrousek1@gmail.com>
2025-07-31 00:16:42 +0200

    release: v1.0.0-beta.44

--- __tests__/e2e/table.spec.js
+++ __tests__/e2e/table.spec.js
@@ -9,14 +9,17 @@ test('Table test', async ({ page, baseURL }) => {
    // ✅ Check that the table element exists
   const table = page.locator('table');
   await expect(table).toBeVisible();
-/*
+
   // ✅ Check that at least one row (excluding thead) exists
   const rows = table.locator('tbody tr');
-  await expect(rows).toHaveCountGreaterThan(0);
+  const rowCount = await rows.count();
+  await expect(rowCount).toBeGreaterThan(0);
 
   // ✅ Check that at least one column (cell) exists in the first row
-  const firstRowCells = rows.nth(0).locator('td');
-  await expect(firstRowCells).toHaveCountGreaterThan(0);*/
+const firstRowCells = rows.nth(0).locator('td');
+const cellCount = await firstRowCells.count();
+expect(cellCount).toBeGreaterThan(0);
+
 });
 
 // npm run test:playwright
